c168f480b65c11bf173af24fd7a6bbfbfd86674a
Divyesh Gohel <hello.dgsolutions@gmail.com>
2020-09-17 10:40:44 +0530

    1709 01

---
+++ example/unit3/NewDemo.cs
@@ -0,0 +1,12 @@
+using System;
+class Program
+{
+    public static void Main()
+    {
+        int x = 10;
+        int y = new int();
+        y = 20;
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+    }
+}
